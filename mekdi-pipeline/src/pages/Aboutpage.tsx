ba798036fed95a2e6c77a277e72561c77ed306af
Clarissa Ch'ng <clarissachnggg@gmail.com>
2025-02-08 20:01:49 +0000

    changed about page design

--- mekdi-pipeline/src/pages/Aboutpage.tsx
+++ mekdi-pipeline/src/pages/Aboutpage.tsx
@@ -11,17 +11,19 @@ const AboutPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1 className="page-title">
+    <div className= "about">
+      <h1 className=" title">
         What is Ethereum?
       </h1>
-      <p className="about-description">
+      <div className="about-description">
+      <p>
       ETH (Ethereum) is like Bitcoin’s cooler, tech-savvy cousin. 🚀 
       It’s not just digital money—it’s also a giant, decentralized 
       computer where people can build apps, create NFTs, and even summon cyber wizards 
       (okay, smart contracts, but close enough). Basically, if Bitcoin is gold, 
       Ethereum is a whole futuristic economy running on magic internet code. 🔥💻
       </p>
+      </div>
 
       <p className="conversation"> 
         Are you feeling more ready to take on the challenge?! 
