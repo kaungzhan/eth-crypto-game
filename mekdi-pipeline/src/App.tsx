58264749c6263e08fb55eee0f8f0c8189eb4122a
ykmhere <zt22740@bristol.ac.uk>
2025-02-08 16:08:00 +0000

    made choosecat page

--- mekdi-pipeline/src/App.tsx
+++ mekdi-pipeline/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/Mainpage";
 import StartPage from "./pages/Startpage";
+import ChooseCatPage from "./pages/Choosecat";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/start" element={<StartPage />} />
+        <Route path="/choose-cat" element={<ChooseCatPage />} />
       </Routes>
     </Router>
   );
